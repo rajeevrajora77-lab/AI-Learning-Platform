57a8cbe4fa57f8cafa7ec36ce0d2534aabd4cbbd
rajeevrajora77-lab <rajeev.rajora.77@gmail.com>
2025-12-25 22:08:13 +0530

    feat: Add __init__.py to make src a Python package

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""Source package for AI Learning Platform"""
+
+__version__ = '1.0.0'
+
+from .content_generator import ContentGenerator
+from .video_generator import VideoGenerator
+
+__all__ = ['ContentGenerator', 'VideoGenerator']
